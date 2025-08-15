4bab374db2d4d6f41d9e1f02b9d2bb6c65e7b09c
longhai shen <bigsea1017@gmail.com>
2025-08-15 17:48:48 +0800

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+export default {
+  // 在 v4 中，content 字段是自动处理的
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config
